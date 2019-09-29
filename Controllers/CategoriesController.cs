acb1b14822d599e90ad12a2ac40506b9ff5cd53a
Cédric David <cedric.david1974@gmail.com>
2019-09-29 22:43:26 +1000

    Add project files.

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Services;
+using Supermarket.API.Resources;
+
+namespace Supermarket.API.Controllers
+{
+    [Route("/api/[controller]")]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CategoryResource>> GetAllAsync()
+        {
+            var categories = await _categoryService.ListAsync();
+            var resources = _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryResource>>(categories);
+
+            return resources;
+        }
+    }
+}
---
+++ Domain/Services/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Supermarket.API.Domain.Models;
+
+namespace Supermarket.API.Domain.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> ListAsync();
+    }
+}
